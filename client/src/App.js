3fc41d367d995af980e6fb065643f419e0c4dca4
hunghly <hunghly@gmail.com>
2020-08-28 11:05:46 -0500

    starting to add components for taskalign

--- client/src/App.js
+++ client/src/App.js
@@ -1,48 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function Client() {
-
-  const accessToken = 'ES4DbzavP6Jr7bNyRMtczxpyKKKGEs';
-  console.log("hello there");
+  const accessToken = "ES4DbzavP6Jr7bNyRMtczxpyKKKGEs";
   const config = {
-    method: 'GET',
+    method: "GET",
     headers: {
-      'Authorization': 'Bearer' + accessToken
+      Authorization: "Bearer " + accessToken,
     },
-  }
+  };
 
-  fetch('http://127.0.0.1:8000/users/', config).then((res) => {
-      console.log("found response");
-      console.log(res);
-      console.log(res.text());
-      // res.json().then((user) => {
-      //   console.log(user);
-      // });
-    });
+  console.log(config.headers);
 
-  return ( <
-    div className = "App" > {
-      /* <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <p>
-                Edit <code>src/App.js</code> and save to reload.
-              </p>
-              <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Learn React
-              </a>
-            </header> */
-    } <
-    h1 > Hello World! < /h1>
+  fetch("http://127.0.0.1:8000/users/", config).then((res) => {
+    console.log("found response");
+    console.log(res);
+    console.log(res.json());
+  });
 
-    <
-    /div>
+  return (
+    <div className="App">
+      <h1> Hello World! </h1>
+    </div>
   );
 }
 
